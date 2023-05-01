29b569c5d4b228f121428839d5fdcf61251ffbb5
Divakar Singh <48847293+divakar166@users.noreply.github.com>
2023-05-01 18:39:19 +0530

    Create swapKth.py

---
+++ swapKth.py
@@ -0,0 +1,10 @@
+# Given an array Arr of size N, swap the Kth element from beginning with Kth element from end.
+
+def swapKth(arr,n,k):
+	arr[k-1],arr[-k] = arr[-k],arr[k-1]
+	return arr
+
+arr = [1,2,3,4,5,6,7,8]
+n = len(arr)
+k = 3
+print(swapKth(arr,n,k))
